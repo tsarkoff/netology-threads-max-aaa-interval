0e6aec59c61b11d48ee31353f90700217119e2df
tsarkoff <tsarkov.vladimir@gmail.com>
2024-04-04 01:06:09 +0300

    For branch "max" : Runnable implementation changed with Callable onr, including ExecutorService.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,61 @@
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
+    private static final int STRINGS_COUNT = 25;
+    private static final int STRING_CHARS_COUNT = 30_000;
+    private static final String CHARS = "aab";
 
-    public static void main(String[] args) throws InterruptedException {
-        String[] texts = new String[25];
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        String[] texts = new String[STRINGS_COUNT];
         for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("aab", 30_000);
+            texts[i] = generateText(CHARS, STRING_CHARS_COUNT);
         }
 
-        List<Thread> threads = new ArrayList<>();
+        List<Future<Integer>> tasks = new ArrayList<>();
+        final ExecutorService threadPool = Executors.newFixedThreadPool(STRINGS_COUNT);
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            threads.add(new Thread(
-                    () -> {
-                        int maxSize = 0;
-                        for (int i = 0; i < text.length(); i++) {
-                            for (int j = 0; j < text.length(); j++) {
-                                if (i >= j) {
-                                    continue;
-                                }
-                                boolean bFound = false;
-                                for (int k = i; k < j; k++) {
-                                    if (text.charAt(k) == 'b') {
-                                        bFound = true;
-                                        break;
+            tasks.add(
+                    threadPool.submit(
+                            () -> {
+                                int maxSize = 0;
+                                for (int i = 0; i < text.length(); i++) {
+                                    for (int j = 0; j < text.length(); j++) {
+                                        if (i >= j) {
+                                            continue;
+                                        }
+                                        boolean bFound = false;
+                                        for (int k = i; k < j; k++) {
+                                            if (text.charAt(k) == 'b') {
+                                                bFound = true;
+                                                break;
+                                            }
+                                        }
+                                        if (!bFound && maxSize < j - i) {
+                                            maxSize = j - i;
+                                        }
                                     }
                                 }
-                                if (!bFound && maxSize < j - i) {
-                                    maxSize = j - i;
-                                }
+                                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                                return maxSize;
                             }
-                        }
-                        System.out.println(text.substring(0, 100) + " -> " + maxSize);
-                    }
-            ));
-            threads.getLast().start();
+                    ));
         }
 
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        int maxInterval = 0;
+        for (Future<Integer> task : tasks) {
+            Integer currInterval = task.get();
+            if (maxInterval < currInterval) {
+                maxInterval = currInterval;
+            }
         }
+        threadPool.shutdown();
 
+        System.out.println("Max 'aaa' interval from all strings is : " + maxInterval);
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + "ms");
     }
